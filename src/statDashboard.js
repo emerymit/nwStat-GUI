3f467176488928146337df0c632e297889d63400
emerymit <red_emery@hotmail.com>
2020-07-03 18:24:09 -0400

    refactored stats dashboard to allow for better use of components, and enable paramter passing. Took all props up a level

--- src/statDashboard.js
+++ src/statDashboard.js
@@ -1,19 +1,110 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 import GetNavBar from './NwNavBar'
-import AverageDownLabel from './StatsComponents/AverageDownLabel'
-import AveragePingLabel from './StatsComponents/AveragePingLabel'
-import AverageUpLabel from './StatsComponents/AverageUpLabel'
-import TFourAvgDown from './StatsComponents/TFourAverageDown'
-import TFourAvgUp from './StatsComponents/TFourAverageUp'
-import TFourAvgPing from './StatsComponents/TFourAveragePing'
+// import AverageDownLabel from './StatsComponents/AverageDownLabel'
+// import AveragePingLabel from './StatsComponents/AveragePingLabel'
+// import AverageUpLabel from './StatsComponents/AverageUpLabel'
+// import TFourAvgDown from './StatsComponents/TFourAverageDown'
+// import TFourAvgUp from './StatsComponents/TFourAverageUp'
+// import TFourAvgPing from './StatsComponents/TFourAveragePing'
 
 import './styles/statsDashHeaderStyle.css'
 
-class StatDashboard extends React.Component{
-    constructor(props){
-        super(props);
+function StatDashboard(){
+    function formatBitsPerSecond(speed){
+        speed = speed / 1000000
+        return speed
     }
-    render(){
+    //average down all time
+    const [avgDown, setAvgDown] = useState(0);
+    useEffect(() => {
+        //hit the end point
+        fetch('http://192.168.1.244:3001/AvgDown')
+            .then(response =>  {
+                //handle the response
+                return response.json();
+            })
+            .then(data => {
+                //set the prop
+                setAvgDown(formatBitsPerSecond(data[0].avg).toFixed(2))
+            });
+    },[]);
+    //average up all time
+    const [avgUp, setAvgUp] = useState(0);
+    useEffect(() => {
+        //hit the end point
+        fetch('http://192.168.1.244:3001/AvgUp')
+            .then(response =>  {
+                //handle the response
+                return response.json();
+            })
+            .then(data => {
+                //set the prop
+                setAvgUp(formatBitsPerSecond(parseFloat(data[0].avg)).toFixed(2))
+            });
+    },[]);
+    //average ping all time
+    const [avgPing, setAvgPing] = useState(0);
+    useEffect(() => {
+        //hit the end point
+        fetch('http://192.168.1.244:3001/AvgPing')
+            .then(response =>  {
+                //handle the response
+                return response.json();
+            })
+            .then(data => {
+                //set the prop
+                // console.log(typeof(parseInt(data[0].avg)))
+                setAvgPing(parseFloat(data[0].avg).toFixed(2))
+            });
+    },[]);
+    //average down last 24
+    const [avgTFDown, setAvgTFDown] = useState(0);
+    useEffect(() => {
+        //hit the end point
+        fetch('http://192.168.1.244:3001/YdAvgDown')
+            .then(response =>  {
+                //handle the response
+                return response.json();
+            })
+            .then(data => {
+                //set the prop
+                setAvgTFDown(formatBitsPerSecond(data[0].avg).toFixed(2))
+            });
+    },[]);
+    //average up last 24
+    const [avgTFUp, setAvgTFUp] = useState(0);
+
+    useEffect(() => {
+        //hit the end point
+        fetch('http://192.168.1.244:3001/YdAvgUp')
+            .then(response =>  {
+                //handle the response
+                return response.json();
+            })
+            .then(data => {
+                //set the prop
+                setAvgTFUp(formatBitsPerSecond(parseFloat(data[0].avg)).toFixed(2))
+            });
+    },[]);
+    //Average ping last 24
+    const [avgTFPing, setAvgTFPing] = useState(0);
+
+    useEffect(() => {
+        //hit the end point
+        fetch('http://192.168.1.244:3001/YdAvgPing')
+            .then(response =>  {
+                //handle the response
+                return response.json();
+            })
+            .then(data => {
+                //set the prop
+                setAvgTFPing(parseFloat(data[0].avg).toFixed(2))
+            });
+    },[]);
+
+
+
+
         return(
         <div>
             {GetNavBar()}
@@ -29,16 +120,16 @@ class StatDashboard extends React.Component{
 
                     <tr>
                         <td id="rowCap">All Time Average </td>
-                        <td><AverageDownLabel/></td>
-                        <td><AverageUpLabel/></td>
-                        <td><AveragePingLabel/></td>
+                        <td>{avgDown}</td>
+                        <td>{avgUp}</td>
+                        <td>{avgPing}</td>
                     </tr>
 
                     <tr>
                         <td id='rowCap'>Average Over Last 24hrs</td>
-                        <td><TFourAvgDown/></td>
-                        <td><TFourAvgUp/></td>
-                        <td><TFourAvgPing/></td>
+                        <td>{avgTFDown}</td>
+                        <td>{avgTFUp }</td>
+                        <td>{avgTFPing}</td>
                     </tr>
 
                 </tbody>
@@ -48,7 +139,6 @@ class StatDashboard extends React.Component{
             
         </div>
         );
-    }
+    
 }
-
 export default StatDashboard;
