5879bc490add1d05dcedb5853646ada04566f926
emerymit <red_emery@hotmail.com>
2020-07-15 15:13:36 -0400

    fixed small navigation bug

--- src/NwNavBar.js
+++ src/NwNavBar.js
@@ -4,7 +4,7 @@ import {Navbar, Nav, NavDropdown } from 'react-bootstrap'
 function GetNavBar(props) {
     return (
             <Navbar bg="dark" variant="dark" expand="lg">
-            <Navbar.Brand href="#home">Network Stats:5155</Navbar.Brand>
+            <Navbar.Brand href="/">Network Stats:5155</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
